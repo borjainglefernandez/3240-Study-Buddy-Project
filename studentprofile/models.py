0167d912ce251fb59d2edf1c193e406c77fa72d4
borjainglefernandez <65180311+borjainglefernandez@users.noreply.github.com>
2020-10-03 00:02:33 -0400

    Created MVP for Profile Creation

--- studentprofile/models.py
+++ studentprofile/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Student(models.Model):
+
+    # Attach user as part of the model so that the user stores this information
+    # in their profile
+    user = models.OneToOneField(User,
+                                on_delete = models.CASCADE) #If user is deleted, all fields are too
+
+    name = models.CharField(max_length=100) # Field for user's name
+
+    year = models.IntegerField(default = 1) # Field for user's year
+
+    major = models.CharField(max_length=100, default = "None") # Field for user's major
+
+    def __str__(self):
+        return self.user.username

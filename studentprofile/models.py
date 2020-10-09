66302da2f62bd5f47e1c99d502b975bf4fddaa08
Jason Watkins <jmw4dx@virginia.edu>
2020-10-09 15:14:40 -0400

    In-progress attempt to add schedule to student

--- studentprofile/models.py
+++ studentprofile/models.py
@@ -34,5 +34,7 @@ class Student(models.Model):
 
     major = models.CharField(max_length=100, default = "None") # Field for user's major
 
+    schedule = models.OneToOneField(Schedule, on_delete=models.DO_NOTHING, default=None)
+
     def __str__(self):
         return self.user.username

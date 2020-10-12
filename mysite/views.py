14fce3fc9df540d57d8223b99467d1ee4407289d
LM <lm5dc@virginia.edu>
2020-10-12 19:46:51 -0400

    Connects the profile making with schedule making

--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,11 @@
 # Watkins, jmw4dx
 from django.http import HttpResponseRedirect, HttpResponse
-from studentprofile.models import Student
+from studentprofile.models import Student, Schedule, Course, Class
 from django.contrib.auth.models import User
 from django.views import generic
 from django.urls import reverse
+from django.template import loader
+from django.shortcuts import render, get_object_or_404 
 
 
 def home(request):
@@ -20,7 +22,7 @@ def submit_profile(request):
     student.save()
 
     # Redirect to the student profile page
-    return HttpResponseRedirect(reverse('student profile'))
+    return HttpResponseRedirect(reverse('studentprofile:newSchedule'))
 
 class ProfileView(generic.TemplateView):
     model = Student

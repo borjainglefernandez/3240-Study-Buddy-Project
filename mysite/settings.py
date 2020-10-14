4a0f3831248a1081da2b7e988e870dc546f7b91a
Mara Hart <maralihart@gmail.com>
2020-10-13 20:46:21 -0400

    Deleted comments

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,11 +125,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-# import os
-# PROJECT_DIR=os.path.dirname(__file__)
-# STATIC_ROOT = os.path.join(PROJECT_DIR,'static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),

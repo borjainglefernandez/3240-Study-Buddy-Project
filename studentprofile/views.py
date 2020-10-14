3fa7ffc36f0d40389c4872239fdb3f411afbc96f
borjainglefernandez <65180311+borjainglefernandez@users.noreply.github.com>
2020-10-14 13:21:19 -0400

    Switched profile creation to myprofile page and made better error messages

--- studentprofile/views.py
+++ studentprofile/views.py
@@ -36,37 +36,49 @@ def make(request):
 
     # Error for if class number is not a digit
     class_number_digit_error = render(request, 'studentprofile/schedule.html', {
-        'error_message': "Class number must be a digit",
+        'error_message': "Class number must be a digit.",
         'num': num
     })
 
     # Error for if class number is not 4 digits
     class_number_length_error = render(request, 'studentprofile/schedule.html', {
-        'error_message': "Class number must be 4 digits",
+        'error_message': "Class number must be 4 digits.",
         'num': num
     })
 
     # Error for if class is not formatted correctly
     class_input_error = render(request, 'studentprofile/schedule.html', {
-        'error_message': "Class must be a course mnemonic (i.e. CS) followed by a space followed by a 4 digit number (i.e. 3240)",
+        'error_message': "Class must be a course mnemonic (i.e. CS) followed by a space followed by a 4 digit number (i.e. 3240).",
         'num': num
     })
 
     # Error for if strength is not a digit
     strength_digit_error = render(request, 'studentprofile/schedule.html', {
-        'error_message': "Strength must be a digit",
+        'error_message': "Strength must be a digit.",
         'num': num
     })
 
     # Error for if strength is not a digit between 1 and 5
     strenth_range_error = render(request, 'studentprofile/schedule.html', {
-        'error_message': "Strength must be a digit between 1 and 5",
+        'error_message': "Strength must be a digit between 1 and 5.",
         'num': num
     })
 
     # Error for if a course inputted does not exist
     course_does_not_exist_error = render(request, 'studentprofile/schedule.html', {
-        'error_message': "One or more courses inputted does not exist",
+        'error_message': "One or more courses inputted does not exist.",
+        'num': num
+    })
+
+    # Error for if there are not enough classes inputted
+    not_enough_classes_error = render(request, 'studentprofile/schedule.html', {
+        'error_message': "You are missing some required course fields.",
+        'num': num
+    })
+
+    # Error for if there are not enough strengths inputted
+    not_enough_strengths_error = render(request, 'studentprofile/schedule.html', {
+        'error_message': "You are missing some required strength fields.",
         'num': num
     })
 
@@ -79,12 +91,19 @@ def make(request):
     strengths = []
     for i in range(len(classKeys)):
         good = True
-        # print(i)
+
+        # If one or more strengths inputted are not in the 1-5 range, raise an error
         if request.POST[strengthKeys[i]].isdigit():
             if not (1 <= int(request.POST[strengthKeys[i]]) <= 5):
                 print(request.POST[strengthKeys[i]], "is not in range")
                 good = False
                 return strenth_range_error
+
+        # If one or more strengths inputted are blank, raise an error
+        elif request.POST[strengthKeys[i]] == '':
+            return not_enough_strengths_error
+
+        # If one or more strengths inputted are not digits, raise an error
         else:
             print(request.POST[strengthKeys[i]], "is not a digit")
             good = False
@@ -97,11 +116,14 @@ def make(request):
         if len(parts) == 2:
             mn = parts[0]
             num = parts[1]
+
+            # If the number inputted for the class is not 4 digits, raise an error
             if len(num) != 4:
                 print(num, "is not a correct length")
                 good = False
                 return class_number_length_error
 
+            # If the number inputted for the class is not a digit, raise an error
             elif not num.isdigit():
                 print(num, "is not a digit")
                 good = False
@@ -112,10 +134,18 @@ def make(request):
                     go = Course.objects.get(mnemonic=mn, number=int(num))
                     courses.append(go)
                     strengths.append(int(request.POST[strengthKeys[i]]))
+
+                # If the inputted course is in a valid format but does not exist, raise an error
                 except Course.DoesNotExist:
                     print("Failed to find", mn, num)
                     good = False
                     return course_does_not_exist_error
+
+        # If one or more class field is blank, raise an error
+        elif request.POST[classKeys[i]] == '':
+            return not_enough_classes_error
+
+        # If class is not formatted correctly, raise an error
         else:
             print(request.POST[classKeys[i]], "did not split well")
             good = False

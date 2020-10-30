519748b7f9cadea0c889b36160f0bf40899b7d54
Jason Watkins <jmw4dx@virginia.edu>
2020-10-30 13:20:22 -0400

    Setting up view file, making new branch

--- studygroups/views.py
+++ studygroups/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from studentprofile.models import Schedule, Course, Class, Student
+from django.contrib.auth.models import User
 
 # Create your views here.
+def makeGroup(request):
+
+    return HttpResponseRedirect(reverse('student profile'))

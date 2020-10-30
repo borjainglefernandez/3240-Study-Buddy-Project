822f439d5ecb71582943ae59c76505e80898c5a5
Jason Watkins <jmw4dx@virginia.edu>
2020-10-30 16:18:40 -0400

    edited comments

--- studygroups/views.py
+++ studygroups/views.py
@@ -11,14 +11,12 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def makeGroup(request):
-    user = User.objects.get(pk=request.user.id)
 
     # Error for if user name is null and the user is editing their profile
     null_name_error = render(request, 'studygroups/groupCreate.html', {  # Redirects the user to the profile page again
         'error_message': "Group name cannot be blank.",  # Description for the error message displayed
     })
 
-    student = None
     try:
         student = Student.objects.get(user=request.user)
         if request.POST["Name"].strip() == "":
@@ -26,11 +24,11 @@ def makeGroup(request):
     except:
         return HttpResponseRedirect(reverse('home'))
 
-    #print("start of creation")
+
     studyGroup = StudyGroup(name=request.POST["Name"], maxSize=request.POST["Size"])
     studyGroup.save()
     studyGroup.members.add(student)
     studyGroup.save()
-    #print("end of creation")
+
 
     return HttpResponseRedirect(reverse('home'))

d74da549b1c11b9a8f8ebedea643915e1331a20b
LM <lm5dc@virginia.edu>
2020-11-02 18:55:11 -0500

    fixed a bug in urls

--- studygroups/urls.py
+++ studygroups/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 app_name = "studygroups"
 
 urlpatterns = [
-    path('groups/create/', TemplateView.as_view(template_name = "studygroups/groupCreate.html"), name='newGroup'),
+    path('groups/create', TemplateView.as_view(template_name = "studygroups/groupCreate.html"), name='newGroup'),
     path('groups/create/make', views.makeGroup, name='generateGroup'),
     path('groups/join', views.joinGroup, name='joinGroup'),
     path('groups/leave', views.leaveGroup, name='leaveGroup'),
